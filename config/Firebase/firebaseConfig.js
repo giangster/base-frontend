f87124a43343ba9d67e7b317fb410849a2f120c2
Giang <thuygiang152@gmail.com>
2020-03-23 21:05:32 +0200

    Firebase config

---
+++ config/Firebase/firebaseConfig.js
@@ -0,0 +1,10 @@
+export const firebaseConfig = {
+  apiKey: "AIzaSyBTWRIzH_m_YVidkSTx3trD4vxhnvF_UuA",
+  authDomain: "base-69690.firebaseapp.com",
+  databaseURL: "https://base-69690.firebaseio.com",
+  projectId: "base-69690",
+  storageBucket: "base-69690.appspot.com",
+  messagingSenderId: "363416682592",
+  appId: "1:363416682592:web:6fc3073b581de35a958305",
+  measurementId: "G-WLTLV1DVE1"
+};
